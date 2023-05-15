d39faf7691d75a812b59013a947beff1a728ccb9
rihid <rizalhidayat006@gmail.com>
2023-05-15 12:48:13 +0700

    Post new pelanggan data

--- buku-pelanggan-app/src/forms/AddForm.js
+++ buku-pelanggan-app/src/forms/AddForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const AddForm = () => {
+const AddForm = (props) => {
     const initialFormState = [
         {
             id: null,
@@ -12,54 +12,67 @@ const AddForm = () => {
             alamat: "",
         },
     ];
-    const [dataNew, setDataNew] = useState(initialFormState);
+
+    const [dataAdd, setDataAdd] = useState(initialFormState);
+
+    // Handle Submit Data
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setDataAdd({...dataAdd, [name]: value});
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addData(dataAdd)
+        setDataAdd(initialFormState);
+    }
+
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div className="sm:col-span-4">
                 <label htmlFor="nama-pelanggan" 
                 className="block text-sm font-bold leading-6 text-gray-900">Nama Pelanggan</label>
                 <div className="mt-2">
-                    <input type="text" 
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="nama" value={dataAdd.name} 
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} />
                 </div>
             </div>
             <div className="sm:col-span-4">
                 <label htmlFor="nama-bisnis" 
                 className="block text-sm font-bold leading-6 text-gray-900">Nama Bisnis</label>
                 <div className="mt-2">
-                    <input type="text" 
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="bisnisName" value={dataAdd.bisnisName}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} />
                 </div>
             </div>
             <div className="sm:col-span-4">
                 <label htmlFor="kontak" 
                 className="block text-sm font-bold leading-6 text-gray-900">Kontak</label>
                 <div className="mt-2">
-                    <input type="text" 
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="kontak" value={dataAdd.kontak} 
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} />
                 </div>
             </div>
             <div className="sm:col-span-4">
                 <label htmlFor="email" 
                 className="block text-sm font-bold leading-6 text-gray-900">Email</label>
                 <div className="mt-2">
-                    <input type="text" 
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input type="email" name="email" value={dataAdd.email} 
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} />
                 </div>
             </div>
             <div className="sm:col-span-4">
                 <label htmlFor="website" 
                 className="block text-sm font-bold leading-6 text-gray-900">Website</label>
                 <div className="mt-2">
-                    <input type="text" 
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="website" value={dataAdd.website}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} />
                 </div>
             </div>
             <div className="sm:col-span-4">
                 <label htmlFor="Alamat" 
                 className="block text-sm font-bold leading-6 text-gray-900">Alamat</label>
                 <div className="mt-2">
-                    <textarea name="" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                    <textarea name="alamat" value={dataAdd.alamat} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange} ></textarea>
                 </div>
             </div>
             <div className="mt-6 flex items-center justify-end gap-x-6">
