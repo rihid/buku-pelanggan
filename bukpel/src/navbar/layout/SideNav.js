15421f0ec2001be6ffc3f578e3251af31ba2cd48
rihid <rizalhidayat006@gmail.com>
2023-05-23 19:07:49 +0700

    Fix some stuff

--- bukpel/src/navbar/layout/SideNav.js
+++ bukpel/src/navbar/layout/SideNav.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 export default function SideNav() {
 	return(
 		<div className="bg-white h-screen px-6">
-			<div className="px-[1rem] py-4 flex items-center justify-left border-b-[1px] border-inherit text-black">
-				<h1 className="text-[1.25rem] leading-[1.25rem] font-extrabold cursor-pointer">Admin</h1>
+			<div className=" py-4 flex items-center justify-left border-b-[1px] border-inherit text-black">
+				<h1 className="text-[1.25rem] leading-[1.25rem] font-extrabold cursor-pointer">Buku Pelanggan</h1>
 			</div>
 			<div className="flex items-center justify-left gap-[1rem] border-b-[1px] border-inheriet py-3 text-black">
 				<FaHome />
--- bukpel/src/navbar/layout/TopNav.js
+++ bukpel/src/navbar/layout/TopNav.js
@@ -1,26 +1,26 @@
 import React from 'react';
+import avatar from './../../assets/ava.jpg'
 import { FaSearch, FaPlusCircle } from "react-icons/fa";
 
 export default function TopNav(props) {
     return(
-        <div className="flex items-center justify-between h-[4.5rem] shadow-sm px-6 bg-white">
+        <div className="flex items-center justify-between gap-12 h-20 shadow-sm px-6 bg-white">
             <div className="flex items-center">
-                <div className="flex items-center h-[2rem] px-[.9rem] gap-2 bg-[#D9D9D9] rounded-full w-96">
+                <div className="flex items-center h-8 px-[.9rem] gap-2 bg-[#D9D9D9] rounded-full w-full">
                     <FaSearch color='#222426' />
                     <input type="text" placeholder="Telusuri Pelanaggan..." className="outline-none bg-[#D9D9D9] w-96" />
                 </div>
             </div>
-            <div className="flex items-center gap-[2rem]">
+            <div className="flex items-center gap-8">
                 <div>
-                    <button className="flex items-center gap-[.5rem] rounded-[.25rem] bg-[#008EEA] text-white h-[2rem] px-[.9rem]" onClick={() => {return props.setShow(true)}} >
+                    <button className="flex items-center gap-4 rounded-full bg-[#008EEA] text-white h-8 px-[.9rem]" onClick={() => {return props.setShow(true)}} >
                         <span><FaPlusCircle/></span>
                         <span className="font-semibold">Tambah</span>
                     </button>
                 </div>
-                <div>
-                    <p>Nama</p>
+                <div className='flex items-center'>
                     <div>
-                        <img src="./img.png" alt="" />
+                        <img src={avatar} alt="" className='object-contain w-8' />
                     </div>
                 </div>
             </div>
