f01a01da7843501564d95799abd04c38289d0a36
rihid <rizalhidayat006@gmail.com>
2023-05-30 16:16:40 +0700

    update deployment

--- bukpel/src/context/index.js
+++ bukpel/src/context/index.js
@@ -3,11 +3,14 @@ import React, { createContext, useState } from 'react'
 export const Context = createContext()
 
 export const Provider = ({ children }) => {
-  const [searchField, setSearchField] = useState('')
+  const [searchField, setSearchField] = useState('');
+  const [isActive, setActive] = useState(true)
 
   const value = {
     searchField,
     setSearchField,
+    isActive,
+    setActive,
   }
 
   return <Context.Provider value={value}>{children}</Context.Provider>
--- bukpel/src/navbar/layout/SideNav.js
+++ bukpel/src/navbar/layout/SideNav.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 export default function SideNav() {
 	return(
 		<div className="bg-white h-screen px-6 ">
-			<div className="flex items-center justify-left gap-4 py-3 text-black">
+			<div className="flex items-center justify-left gap-4 py-4 text-black">
 				<FaHome />
 				<Link to={'/'}>
 					<p className="text-[1rem] leading-[1.25rem] font-light">Home</p>	
 				</Link>
 			</div>
-			<div className="flex items-center justify-left gap-4 py-3 text-black">
+			<div className="flex items-center justify-left gap-4 py-4 text-black">
 				<FaUsers />
 				<Link to={'pelanggan'}>
 					<p className="text-[1rem] leading-[1.25rem] font-light">Pelanggan</p>
